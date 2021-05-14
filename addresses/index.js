cfa689aaea45ecf0626d74cc1101c6e1f841da84
Soon Lai <soonlai814@gmail.com>
2021-05-14 08:04:31 +0800

    Added Deployment Configuration

--- addresses/index.js
+++ addresses/index.js
@@ -5,8 +5,9 @@ module.exports = {
         USDT: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
     },
     kovan: {
-        DVG: "",
-        UniswapV2Router02: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+        DVG: "0xea9726eFc9831EF0499fD4Db4Ab143F15a797673",
+        UniswapV2Router02: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+        USDT: "0x07de306ff27a2b630b1141956844eb1552b956b5"
     },
     rinkeby: {
         DVG: "",
