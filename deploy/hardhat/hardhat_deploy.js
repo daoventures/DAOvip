cd6ee200835289ae6cb32c5dfb451f2f68dbf133
Lim Keng Hin <kenghin_lim@hotmail.com>
2021-07-15 10:15:11 +0800

    feat: Update deployment for proxy

--- deploy/hardhat/hardhat_deploy.js
+++ deploy/hardhat/hardhat_deploy.js
@@ -6,16 +6,31 @@ const dvgAddress = network_.DVG;
 const uniswapV2Router02Address = network_.UniswapV2Router02;
 const walletAddress = process.env.WALLET_ADDRESS; */
 
-module.exports = async ({deployments}) => {
-  const { deploy } = deployments;
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy, catchUnknownSigner } = deployments;
   const [deployer] = await ethers.getSigners();
 
-  const IMPL = await ethers.getContractFactory("xDVD")
-  const impl = await upgrades.deployProxy(IMPL,[dvgAddress])
+  // const IMPL = await ethers.getContractFactory("xDVD");
+  // const impl = await upgrades.deployProxy(IMPL, [dvgAddress]);
+  let xDVD;
 
-  await impl.deployed() 
-  
-  console.log("Proxy xDVD address", impl.address)
-}
+  await catchUnknownSigner(
+    (xDVD = deploy("xDVD", {
+      from: deployer.address,
+      proxyContract: "OpenZeppelinTransparentProxy",
+      proxy: {
+        owner: deployer.address, // Owner of proxy
+        methodName: "initialize", // Method to execute when deploying proxy
+      },
+      args: [dvgAddress],
+    }))
+  );
+
+  // you could pause the deployment here and wait for input to continue
+
+  // await xDVD.deployed();
+
+  console.log("Proxy xDVD address", xDVD.address);
+};
 
 module.exports.tags = ["hardhat_deploy"];
