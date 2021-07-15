f49865681e06b511f02e31667510f6ab6f714fdc
Lim Keng Hin <kenghin_lim@hotmail.com>
2021-07-15 10:14:16 +0800

    chore: :art: Format DVD Contract

--- contracts/xDVD.sol
+++ contracts/xDVD.sol
@@ -7,30 +7,34 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-
 // This contract handles swapping to and from xDVD, DAOventures's vip token
-contract xDVD is ERC20Upgradeable{
+contract xDVD is ERC20Upgradeable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     struct User {
-        uint tier;
-        uint amountDeposited;
+        uint256 tier;
+        uint256 amountDeposited;
     }
 
     IERC20 public dvd;
 
-    uint[] tierAmount;
+    uint256[] tierAmount;
     mapping(address => User) user;
 
-    event Deposit(address indexed user, uint256 dvdAmount, uint256 xDVDAmount);
-    event Withdraw(address indexed user, uint256 dvdAmount, uint256 xDVDAmount);
+    event Deposit(address indexed user, uint256 DVDAmount, uint256 xDVDAmount);
+    event Withdraw(address indexed user, uint256 DVDAmount, uint256 xDVDAmount);
 
     //Define the DVD token contract
-    function initialize(IERC20 _dvd) external initializer{
+    function initialize(address _dvd) external initializer {
         __ERC20_init("VIP DVD", "xDVD");
-        dvd = _dvd;
-        tierAmount = [1000 * 1e18, 10_000 * 1e18, 50_000* 1e18, 100_000 * 1e18];
+        dvd = IERC20(_dvd);
+        tierAmount = [
+            1000 * 1e18,
+            10_000 * 1e18,
+            50_000 * 1e18,
+            100_000 * 1e18
+        ];
     }
 
     // constructor(IERC20 _dvd) {
@@ -48,7 +52,7 @@ contract xDVD is ERC20Upgradeable{
         if (totalShares == 0) {
             what = _amount;
             _mint(msg.sender, _amount);
-        } 
+        }
         // Calculate and mint the amount of xDVD the DVD is worth. The ratio will change overtime
         else {
             what = _amount.mul(totalShares).div(totalDVD);
@@ -57,7 +61,9 @@ contract xDVD is ERC20Upgradeable{
         // Lock the DVD in the contract
         dvd.safeTransferFrom(msg.sender, address(this), _amount);
 
-        user[msg.sender].amountDeposited = user[msg.sender].amountDeposited.add(_amount);
+        user[msg.sender].amountDeposited = user[msg.sender].amountDeposited.add(
+            _amount
+        );
         user[msg.sender].tier = calculateTier(user[msg.sender].amountDeposited);
 
         emit Deposit(msg.sender, _amount, what);
@@ -68,36 +74,57 @@ contract xDVD is ERC20Upgradeable{
         // Gets the amount of xDVD in existence
         uint256 totalShares = totalSupply();
         // Calculates the amount of DVD the xDVD is worth
-        uint256 what = _share.mul(dvd.balanceOf(address(this))).div(totalShares);
+        uint256 what = _share.mul(dvd.balanceOf(address(this))).div(
+            totalShares
+        );
         dvd.safeTransfer(msg.sender, what);
 
-        uint _depositedAmount = user[msg.sender].amountDeposited.mul(_share).div(balanceOf(msg.sender));
+        uint256 _depositedAmount = user[msg.sender]
+        .amountDeposited
+        .mul(_share)
+        .div(balanceOf(msg.sender));
 
-        if(what > _depositedAmount) {
-            user[msg.sender].amountDeposited = user[msg.sender].amountDeposited.sub(_depositedAmount);
-            user[msg.sender].tier = calculateTier(user[msg.sender].amountDeposited);
+        if (what > _depositedAmount) {
+            user[msg.sender].amountDeposited = user[msg.sender]
+            .amountDeposited
+            .sub(_depositedAmount);
+            user[msg.sender].tier = calculateTier(
+                user[msg.sender].amountDeposited
+            );
         }
 
         _burn(msg.sender, _share);
         emit Withdraw(msg.sender, what, _share);
     }
 
-    function calculateTier(uint _depositedAmount) internal view returns (uint){
-        if(_depositedAmount <= tierAmount[0]) { //less than or equal to 1000
-            return 0; 
-        } else if(_depositedAmount <= tierAmount[1]) { //less than or equal to 10_000
+    function calculateTier(uint256 _depositedAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        if (_depositedAmount <= tierAmount[0]) {
+            //less than or equal to 1000
+            return 0;
+        } else if (_depositedAmount <= tierAmount[1]) {
+            //less than or equal to 10_000
             return 1;
-        } else if(_depositedAmount <= tierAmount[2]) { //less than or equal to 50_000
+        } else if (_depositedAmount <= tierAmount[2]) {
+            //less than or equal to 50_000
             return 2;
-        }else if(_depositedAmount <= tierAmount[3]) { //less than or equal to 100_000
+        } else if (_depositedAmount <= tierAmount[3]) {
+            //less than or equal to 100_000
             return 3;
         } else {
             return 4;
         }
     }
 
-    function getTier(address _addr) public view returns (uint _tier, uint _depositedAmount) {
-        _tier = user[_addr].tier ;
-        _depositedAmount = user[_addr].amountDeposited; 
+    function getTier(address _addr)
+        public
+        view
+        returns (uint256 _tier, uint256 _depositedAmount)
+    {
+        _tier = user[_addr].tier;
+        _depositedAmount = user[_addr].amountDeposited;
     }
 }
