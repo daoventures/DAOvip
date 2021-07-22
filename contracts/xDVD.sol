d7b65891cf0276f178849c6471a7cb81ac03dc69
lucky83149028 <zhengyun50511@gmail.com>
2021-07-22 19:01:17 +0800

    xDVD updated for the tier snapshot

--- contracts/xDVD.sol
+++ contracts/xDVD.sol
@@ -3,18 +3,25 @@
 pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ArraysUpgradeable.sol";
 
 // This contract handles swapping to and from xDVD, DAOventures's vip token
 contract xDVD is ERC20Upgradeable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
+    using ArraysUpgradeable for uint256[];
 
     struct User {
-        uint256 tier;
+        uint256 tier;  // It's not used in v2
         uint256 amountDeposited;
+    }
+
+    struct TierSnapshots {
+        uint256[] blockNumbers;
+        uint8[] tiers;
     }
 
     IERC20 public dvd;
@@ -22,8 +29,14 @@ contract xDVD is ERC20Upgradeable {
     uint256[] tierAmount;
     mapping(address => User) user;
 
+    //
+    // v2 variables
+    //
+    mapping (address => TierSnapshots) private _accountTierSnapshots;
+
     event Deposit(address indexed user, uint256 DVDAmount, uint256 xDVDAmount);
     event Withdraw(address indexed user, uint256 DVDAmount, uint256 xDVDAmount);
+    event Tier(address indexed user, uint8 prevTier, uint8 newTier);
 
     //Define the DVD token contract
     function initialize(address _dvd) external initializer {
@@ -31,14 +44,23 @@ contract xDVD is ERC20Upgradeable {
         dvd = IERC20(_dvd);
         tierAmount = [
             1000 * 1e18,
-            10_000 * 1e18,
-            50_000 * 1e18,
-            100_000 * 1e18
+            10_000 * 1e18
         ];
     }
 
     // Pay some DVDs. Earn some shares. Locks DVD and mints xDVD
     function deposit(uint256 _amount) public {
+        _deposit(msg.sender, msg.sender, _amount);
+    }
+
+    /**
+     * @dev This function will be called from DAOmine. The msg.sender is DAOmine.
+     */
+    function depositByProxy(address _user, uint256 _amount) public {
+        _deposit(msg.sender, _user, _amount);
+    }
+
+    function _deposit(address _proxy, address _user, uint256 _amount) internal {
         // Gets the amount of DVD locked in the contract
         uint256 totalDVD = dvd.balanceOf(address(this));
         // Gets the amount of xDVD in existence
@@ -47,22 +69,20 @@ contract xDVD is ERC20Upgradeable {
         // If no xDVD exists, mint it 1:1 to the amount put in
         if (totalShares == 0) {
             what = _amount;
-            _mint(msg.sender, _amount);
+            _mint(_proxy, _amount);
         }
         // Calculate and mint the amount of xDVD the DVD is worth. The ratio will change overtime
         else {
             what = _amount.mul(totalShares).div(totalDVD);
-            _mint(msg.sender, what);
+            _mint(_proxy, what);
         }
         // Lock the DVD in the contract
-        dvd.safeTransferFrom(msg.sender, address(this), _amount);
+        dvd.safeTransferFrom(_proxy, address(this), _amount);
 
-        user[msg.sender].amountDeposited = user[msg.sender].amountDeposited.add(
-            _amount
-        );
-        user[msg.sender].tier = calculateTier(user[msg.sender].amountDeposited);
+        user[_user].amountDeposited = user[_user].amountDeposited.add(_amount);
+        _updateSnapshot(_user, user[_user].amountDeposited);
 
-        emit Deposit(msg.sender, _amount, what);
+        emit Deposit(_user, _amount, what);
     }
 
     // Claim back your DVDs. Unclocks the staked + gained DVD and burns xDVD
@@ -81,47 +101,70 @@ contract xDVD is ERC20Upgradeable {
         .div(balanceOf(msg.sender));
 
         
-        user[msg.sender].amountDeposited = user[msg.sender]
-            .amountDeposited
-            .sub(_depositedAmount);
-            
-        user[msg.sender].tier = calculateTier(
-                user[msg.sender].amountDeposited
-            );
-        
+        user[msg.sender].amountDeposited = user[msg.sender].amountDeposited.sub(_depositedAmount);
+        _updateSnapshot(msg.sender, user[msg.sender].amountDeposited);
 
         _burn(msg.sender, _share);
         emit Withdraw(msg.sender, what, _share);
     }
 
-    function calculateTier(uint256 _depositedAmount)
-        internal
-        view
-        returns (uint256)
-    {
+    function _calculateTier(uint256 _depositedAmount) internal view returns (uint8) {
         if (_depositedAmount <= tierAmount[0]) {
-            //less than or equal to 1000
+            // Beginner (0-1k)
             return 0;
         } else if (_depositedAmount <= tierAmount[1]) {
-            //less than or equal to 10_000
+            // Intermediate (1k-10k)
             return 1;
-        } else if (_depositedAmount <= tierAmount[2]) {
-            //less than or equal to 50_000
-            return 2;
-        } else if (_depositedAmount <= tierAmount[3]) {
-            //less than or equal to 100_000
-            return 3;
         } else {
-            return 4;
+            // Legendary (10k and above)
+            return 2;
         }
     }
 
-    function getTier(address _addr)
-        public
-        view
-        returns (uint256 _tier, uint256 _depositedAmount)
-    {
-        _tier = user[_addr].tier;
+    function getTier(address _addr) public view returns (uint256 _tier, uint256 _depositedAmount) {
         _depositedAmount = user[_addr].amountDeposited;
+        _tier = _calculateTier(_depositedAmount);
+    }
+
+    /**
+     * @dev Retrieves the tier of `_addr` at the `_blockNumber`.
+     */
+    function tierAt(address _addr, uint256 _blockNumber) public view returns (uint8) {
+        (bool snapshotted, uint8 tier) = _tierAt(_blockNumber, _accountTierSnapshots[_addr]);
+
+        return snapshotted ? tier : _calculateTier(user[_addr].amountDeposited);
+    }
+
+    function _tierAt(uint256 blockNumber, TierSnapshots storage snapshots) internal view returns (bool, uint8)
+    {
+        uint256 snpashotCount = snapshots.blockNumbers.length;
+        if (snpashotCount == 0) {
+            // Nothing in the snapshot
+            return (false, 0);
+        }
+        if (blockNumber < snapshots.blockNumbers[0] || snapshots.blockNumbers[snpashotCount-1] < blockNumber) {
+            // Out of snapshot range
+            return (false, 0);
+        }
+
+        uint256 index = snapshots.blockNumbers.findUpperBound(blockNumber);
+        return (true, snapshots.tiers[index]);
+    }
+
+    function _updateSnapshot(address _addr, uint256 _depositedAmount) private {
+        uint8 tier = _calculateTier(_depositedAmount);
+        TierSnapshots storage snapshots = _accountTierSnapshots[_addr];
+        if (_lastSnapshotTier(snapshots.tiers) !=  tier) {
+            snapshots.blockNumbers.push(block.number);
+            snapshots.tiers.push(tier);
+        }
+    }
+
+    function _lastSnapshotTier(uint8[] storage _tiers) private view returns (uint8) {
+        if (_tiers.length == 0) {
+            return 0;
+        } else {
+            return _tiers[_tiers.length - 1];
+        }
     }
 }
